3e37be969f650d001f4bbe2f012aedc018f7a59f
Liu Cao <caoliu98@yahoo.com>
2021-09-04 16:16:38 +0800

    sb-internet bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"sb-internet",		60, 4},
-	{"",	"sb-backlight",		0,	5},
-	{"", 	"sb-volume",		0,	3},
-	{"",	"sb-battery",		60,	2},
-	{"", 	"sb-clock",			60,	1},
+    /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
+    {"",    "sb-nettraf",       1,  7},
+    {"",    "sb-internet",      60, 4},
+    {"",    "sb-cpu",           15, 6},
+    {"",    "sb-backlight",     0,  5},
+    {"",    "sb-volume",        0,  3},
+    {"",    "sb-battery",       60, 2},
+    {"",    "sb-clock",         60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
